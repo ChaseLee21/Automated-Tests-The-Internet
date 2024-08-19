fffded42f2a922769b4439fae13bc483756cad92
Tiny Penguin <chase.seeberger@gmail.com>
2024-08-19 12:58:32 -0700

    fix: passing username and password parameters in the url

--- tests/basic-auth.spec.js
+++ tests/basic-auth.spec.js
@@ -4,7 +4,7 @@ const assert = require("assert");
 const { Builder, By, Key } = require("selenium-webdriver");
 
 describe("Basic Authentication", function() {
-  let driver, alert, message;
+  let driver, username, password, message;
 
   beforeEach(async function() {
     driver = await new Builder().forBrowser("chrome").build();
@@ -15,11 +15,10 @@ describe("Basic Authentication", function() {
   });
 
   it("should authenticate my session", async function() {
-    await driver.get("https://the-internet.herokuapp.com/basic_auth");
-    alert = await driver.switchTo().alert()
-    await alert.authenticateAs("admin", "admin");
+    username = "admin";
+    password = "admin";
+    await driver.get(`https://${username}:${password}@the-internet.herokuapp.com/basic_auth`);
     message = await driver.findElement(By.css("p")).getText();
-    console.log(message);
     assert(message.includes("Congratulations! You must have the proper credentials."));
   });
 });
