4b52f777ee6dee945f81ca6c3b6db279f1bc4d5e
Tiny Penguin <chase.seeberger@gmail.com>
2024-08-19 15:10:18 -0700

    feat: expanded challenging dom test

--- tests/challenging-dom.spec.js
+++ tests/challenging-dom.spec.js
@@ -5,11 +5,11 @@ const { Builder, By } = require("selenium-webdriver");
 
 describe("Challenging DOM", function() {
 
-    beforeEach(async function() {
+    this.beforeAll(async function() {
         driver = await new Builder().forBrowser("chrome").build();
     });
     
-    afterEach(async function() {
+    this.afterAll(async function() {
     await driver.quit();
     });
 
@@ -28,7 +28,41 @@ describe("Challenging DOM", function() {
 
     it("should locate the canvas element", async function() {
         await driver.get('https://the-internet.herokuapp.com/challenging_dom')
-        const canvas = driver.findElement(By.css('#content #canvas'))
+        const canvas = await driver.findElement(By.css('#content #canvas'))
         assert.ok(canvas)
+    });
+
+    it("should locate the table element", async function() {
+        await driver.get('https://the-internet.herokuapp.com/challenging_dom')
+        const table = await driver.findElement(By.css('#content table'))
+        assert.ok(table)
+    });
+
+    it("should locate and log the elements in the 6th row of the table", async function() {
+        await driver.get('https://the-internet.herokuapp.com/challenging_dom')
+        const row = await driver.findElements(By.css('#content table tbody tr:nth-child(6) td'))
+        for (let e of row) {
+            console.log(await e.getText());
+        }
+        assert.ok(row)
+    });
+
+    it("should locate and log the elements in the 4th column of the table", async function() {
+        await driver.get('https://the-internet.herokuapp.com/challenging_dom')
+        const tbody = await driver.findElements(By.css('#content table tbody tr'))
+        for (let row of tbody) {
+            let col = await row.findElement(By.css('td:nth-child(4)'))
+            console.log(await col.getText());
+            assert.ok(col)
+        }
+    });
+
+    it("should locate and log the button elements on the left side of the page", async function() {
+        await driver.get('https://the-internet.herokuapp.com/challenging_dom')
+        const buttons = await driver.findElements(By.css('#content .large-2 a'))
+        for (let button of buttons) {
+            console.log(await button.getText());
+        }
+        assert.ok(buttons)
     });
 });
