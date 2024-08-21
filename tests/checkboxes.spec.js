45c77bae52891a62ba6cc5283505a3711f174dff
Tiny Penguin <chase.seeberger@gmail.com>
2024-08-21 10:59:07 -0700

    feat: adding more assertions to checkbox test

--- tests/checkboxes.spec.js
+++ tests/checkboxes.spec.js
@@ -4,7 +4,7 @@ const assert = require("assert")
 const { Builder, By, Key } = require("selenium-webdriver")
 
 describe("Checkboxes", function() {
-    let driver, form, checkboxes;
+    let driver, form, checkboxes, checkbox, checked;
 
     this.beforeAll(async function() {
         driver = await new Builder().forBrowser("chrome").build()
@@ -20,5 +20,37 @@ describe("Checkboxes", function() {
         assert.ok(form)
         checkboxes = await form.findElements(By.css('input'))
         assert.equal(checkboxes.length, 2, `Found ${checkboxes.length} inputs, expected 2.`)
+    });
+
+    it("should read the value of each checkbox", async function() {
+        for (let i = 0; i < checkboxes.length; i++) {
+            checkbox = checkboxes[i]
+            checked = await checkbox.getAttribute('checked')
+            console.log(`Checkbox ${i + 1} value: ${checked}`)
+            if (i===0) assert.equal(checked, null) 
+            else assert.equal(checked, 'true')
+        }
+    });
+
+    it("should check each unchecked box and confirm all are checked", async function() {
+        checkbox, checked = undefined
+        for (let i = 0; i < checkboxes.length; i++) {
+            checkbox = checkboxes[i]
+            checked = await checkbox.getAttribute('checked')
+            if (!checked) checkbox.click()
+            checked = await checkbox.getAttribute('checked')
+            assert.equal(checked, 'true')
+        }
+    });
+
+    it("should uncheck each checked box and confirm all are unchecked", async function() {
+        checkbox, checked = undefined
+        for (let i = 0; i < checkboxes.length; i++) {
+            checkbox = checkboxes[i]
+            checked = await checkbox.getAttribute('checked')
+            if (checked) checkbox.click()
+            checked = await checkbox.getAttribute('checked')
+            assert.equal(checked, null || undefined)
+        }
     });
 });
