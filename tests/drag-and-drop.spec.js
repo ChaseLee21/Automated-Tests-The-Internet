3383a00293290329b4bc4fe24659c9d88bce2f6f
Tiny Penguin <chase.seeberger@gmail.com>
2024-08-26 10:09:27 -0700

    feat: drag and drop testing box order

--- tests/drag-and-drop.spec.js
+++ tests/drag-and-drop.spec.js
@@ -1,5 +1,5 @@
 // filename: placeholder.spec.js
-// https://the-internet.herokuapp.com/
+// https://the-internet.herokuapp.com/drag_and_drop
 const assert = require("assert");
 const { Builder, By, Key } = require("selenium-webdriver");
 
@@ -15,10 +15,26 @@ describe("Drag and Drop", function() {
     });
 
     it("should confirm box A and box B are in order", async function() {
-        
+        await driver.get('https://the-internet.herokuapp.com/drag_and_drop')
+        const expectedOrder = ['A', 'B']
+        const elements = await driver.findElements(By.css('#content #columns .column'))
+        const foundOrder = await Promise.all(elements.map(async (element) => element.getText()))
+        assert.deepStrictEqual(expectedOrder, foundOrder)
     });
 
     it("should drag box A to be on the right of box B", async function() {
-        
+        await driver.get('https://the-internet.herokuapp.com/drag_and_drop')
+
+        // drag box A to location of box B
+        const boxA = await driver.findElement(By.css('#content #columns #column-a'))
+        const boxB = await driver.findElement(By.css('#content #columns #column-b'))
+        const actions = driver.actions({async: true})
+        await actions.dragAndDrop(boxA, boxB).perform()
+
+        // confirm box order
+        const expectedOrder = ['B', 'A']
+        const elements = await driver.findElements(By.css('#content #columns .column'))
+        const foundOrder = await Promise.all(elements.map(async (element) => element.getText()))
+        assert.deepStrictEqual(expectedOrder, foundOrder)
     });
 });
